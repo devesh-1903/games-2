1c35710722befadcb201283cfb99cfb213365109
dshtrikul <dshtrikul@users.noreply.github.com>
2019-03-18 22:31:33 +0200

    Unittest

--- game_blackjack_v4.py
+++ game_blackjack_v4.py
@@ -3,6 +3,7 @@ import random
 from random import shuffle
 
 #test
+#unittest
 
 def ranks(): return ["2", "3", "4", "5", "6", "7","8", "9", "10", "J", "Q", "K", "A"]
 def suits(): return ['♠', '♦', '♥', '♣']
