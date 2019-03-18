aa64eed63aebe9e1e22001b74c2374076cc3a976
dshtrikul <dshtrikul@users.noreply.github.com>
2019-03-18 22:24:52 +0200

    test comment

--- game_blackjack_v4.py
+++ game_blackjack_v4.py
@@ -1,6 +1,9 @@
 import time
 import random
 from random import shuffle
+
+#test
+
 def ranks(): return ["2", "3", "4", "5", "6", "7","8", "9", "10", "J", "Q", "K", "A"]
 def suits(): return ['♠', '♦', '♥', '♣']
 def again():
@@ -10,11 +13,11 @@ def again():
         quit()
     else: return True
 
-class Card: 
+class Card:
     def __init__(self,rank,suit):
         self.rank = rank
         self.suit = suit
-    
+
     def __str__(self):
         return self.rank + '' + self.suit
 
@@ -25,17 +28,17 @@ class Card:
         used=[]
         flop_PC=[]
         flop_USER=[]
-    
+
     def pickrandom(player):
         x = 'start'
         while str(x) not in used or str(x) == 'start' :
             x = card_deck.randomcard()
             used.append(str(x))
-            if player == 'PC':      
+            if player == 'PC':
                 flop_PC.append(str(x))
-            if player == 'USER' : 
+            if player == 'USER' :
                 flop_USER.append(str(x))
-            break   
+            break
         return str(x)
 
     def pointcheck(player):
@@ -51,8 +54,8 @@ class Card:
                 if total > 10: total += 1
                 else : total += 11
         return total
-    
-    def image(rank_suit):   
+
+    def image(rank_suit):
         cardlines = [[] for i in range(11)]
         if len(rank_suit) == 2:
             cardlines[0].append('  ╔══════════════╗')
@@ -82,7 +85,7 @@ class Card:
 
     def mainscreen():
         top = [ [[''] for i in range(11)] for i in range(7) ]          ### made to hol MAX seven cards
-        bottom = [ [[''] for i in range(11)] for i in range(7) ]       
+        bottom = [ [[''] for i in range(11)] for i in range(7) ]
 
         count = 0
         for i in flop_PC:
@@ -98,14 +101,14 @@ class Card:
 
         i = 0
         print('\n   ~~~~~ PC ~~~~~')
-        while i < 11: 
+        while i < 11:
             print(top[0][i][0],top[1][i][0],top[2][i][0],top[3][i][0],top[4][i][0],top[5][i][0],top[6][i][0])
             i += 1
         print('\n   >>>>> Total', Card.pointcheck('PC'),'points')
         print('\n')
         print('\n   ***** YOU *****')
         i = 0
-        while i < 11: 
+        while i < 11:
             print(bottom[0][i][0],bottom[1][i][0],bottom[2][i][0],bottom[3][i][0],bottom[4][i][0],bottom[5][i][0],bottom[6][i][0])
             i += 1
         print('\n   >>>>> Total', Card.pointcheck('USER'),'points')
@@ -114,7 +117,7 @@ class Deck:
     def __init__(self):
         self.contents = []
         self.contents = [Card(rank,suit) for rank in ranks() for suit in suits()]
-            
+
     def __str__(self):
         return 'deck of cards'
 
@@ -122,20 +125,20 @@ class Deck:
         random.shuffle(self.contents)
         return self.contents[0]
 
-class Bank: 
+class Bank:
     def __init__(self, name, balance=0):
         self.name = name
         self.balance = balance
-    
+
     def __str__(self):
         return '{} Balance: {}$'.format(self.name, self.balance)
 
     def deposit(self, amount):
         self.balance += amount
-        
+
     def withdraw(self, amount):
         self.balance -= amount
-            
+
     def pay2winner():
         while True:
             while uservictory == 'draw':
@@ -161,9 +164,9 @@ class Bank:
     def checkbalance():
         print()
         print('   ',USER_account)
-        print('   ',PC_account)   
+        print('   ',PC_account)
 
-        if PC_account.balance == 0 : 
+        if PC_account.balance == 0 :
             print('   PC has 0 $\nThank you for playing')
             quit()
         if USER_account.balance == 0:
@@ -173,13 +176,13 @@ class Bank:
     def takebet():
         global bet
         while True:
-            
-            try: 
+
+            try:
                 bet = int(input('\n   Make your Bet $'))
-            except: 
+            except:
                 print('   Enter a number')
                 continue
-        
+
             if bet > USER_account.balance:
                 print('   Not enough Credit\n   Your balance is: $',USER_account.balance)
                 continue
@@ -189,18 +192,18 @@ class Bank:
             elif bet <= USER_account.balance :
                 print('   Bet',bet,'$ accepted')
                 break
-                              
+
 #*****************************************************************************************
 #*****************************************************************************************
 #*****************************************************************************************
 
 while True:
     balance = input('\n   Please provide Deposit amount $: ')     # Deposit loop
-    try: 
+    try:
         balance = int(balance)
         break
     except: print('   Enter a number')
-                
+
 print('   PC Deposits same amount : $',balance)
 PC_account = Bank('PC', balance)
 USER_account = Bank('USER', balance)
@@ -208,7 +211,7 @@ USER_account = Bank('USER', balance)
 card_deck = Deck()
 
 while True:
-    while True: 
+    while True:
         uservictory = True
         Bank.takebet()
         Card.emptyhands()
@@ -221,18 +224,18 @@ while True:
         input('\n   Press any key to deal\n')
         time.sleep(0.2)
         Card.mainscreen()
-        
+
         ###################################### PLAYER TURN
-        
-        while True:   
+
+        while True:
             hit_or_stand = input('\n   press ENTER to hit, type S to STAND : ')
             print('   ===========================\n')
-            if hit_or_stand.upper() == 'S': 
+            if hit_or_stand.upper() == 'S':
                 if Card.pointcheck('PC') > Card.pointcheck('USER'):   # if you stand while loosing in point
                     uservictory = False
-                break 
-        
-            if len(hit_or_stand) >= 0 : 
+                break
+
+            if len(hit_or_stand) >= 0 :
                 Card.pickrandom('USER')
                 Card.mainscreen()
                 if Card.pointcheck('USER') > 21:                      # if >21 BUST
@@ -246,10 +249,10 @@ while True:
 
         ###################################### COMPUTER TURN
 
-        while uservictory:   
-                                                   
-            while Card.pointcheck('PC') <= Card.pointcheck('USER') :  
-                
+        while uservictory:
+
+            while Card.pointcheck('PC') <= Card.pointcheck('USER') :
+
                 if Card.pointcheck('PC') == Card.pointcheck('USER') and Card.pointcheck('PC') > 17:
                     print('\n      ~~~ It is a DRAW ~~~')
                     uservictory = 'draw'
@@ -257,7 +260,7 @@ while True:
 
                 print('   >>>>>>PC requests to Hit')
                 Card.pickrandom('PC')
-                
+
                 if Card.pointcheck('PC') > 21:
                     Card.mainscreen()
                     print('\n      ~~~ PC is BUSTED ~~~')                   # if >21 PC BUST
@@ -270,18 +273,7 @@ while True:
                     break
             break
         ###################################### WIN / LOSE
-      
+
         if Bank.pay2winner() : break        # adjusts balance according to Bet
     Bank.checkbalance()                     # prints new balance, quits if == 0
     if again() : continue                   # asks to play again
-
-
-
-
-
-
-
-
-
-
-
